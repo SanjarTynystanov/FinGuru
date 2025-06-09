d6e5eec165eed411ec1965cee6473f9b717821bc
Sanjar <tynystanov.s.u@example.com>
2025-06-09 13:37:51 +0600

    Исправлен запуск через переменную PORT

--- app.py
+++ app.py
@@ -150,4 +150,6 @@ def calculate():
         return jsonify({'error': 'Income and expenses must be numbers'}), 400
 
 if __name__ == '__main__':
-      app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
